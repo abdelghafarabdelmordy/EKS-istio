d8d48c22fc79bbf2968ad6fccd3a96076157b855
Abdelghafar Abdelmordy <abdelghafar.abdelmordy@pearson.com>
2024-12-24 15:27:10 +0200

    chnage1

--- module/variables.tf
+++ module/variables.tf
@@ -49,6 +49,6 @@ variable "spot_instance_types" {}
 variable "desired_capacity_on_demand" {}
 variable "min_capacity_on_demand" {}
 variable "max_capacity_on_demand" {}
-variable "desired_capacity_spot" {}
-variable "min_capacity_spot" {}
-variable "max_capacity_spot" {}
+#variable "desired_capacity_spot" {}
+#variable "min_capacity_spot" {}
+#variable "max_capacity_spot" {}

f35a54fa53c4d469d2f3b325a7ae4ad244adc0e5
Abdelghafar Abdelmordy <abdelghafar.abdelmordy@pearson.com>
2024-12-25 12:34:05 +0200

    add jenkins server

--- Jenkins-Server/data.tf
+++ Jenkins-Server/data.tf
@@ -17,22 +17,22 @@ data "aws_ami" "ubuntu" {
   }
 
   owners = ["099720109477"] # Canonical's AWS account ID
-
 }
+
+data "aws_vpc" "vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["*vpc*"] # Replace with your VPC name or tag
+  }
+}
+
 data "aws_subnets" "public_subnets" {
   filter {
     name   = "tag:Name"
     values = ["*subnet-public*"] # Replace with your specific naming convention
   }
-
   filter {
     name   = "vpc-id"
     values = [aws_vpc.vpc.id] # Replace with your VPC ID reference
-  }
-}
-data "aws_vpc" "vpc" {
-  filter {
-    name   = "tag:Name"
-    values = ["*vpc*"] # Replace with your VPC name or tag
   }
 }
